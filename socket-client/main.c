7b022f35af51d80028ad35c2a41d58f438cdd557
paranjaydubey <paranjaydubey19@gmail.com>
2024-07-11 09:39:21 +0530

    socket-client updated

--- socket-client/main.c
+++ socket-client/main.c
@@ -1,7 +1,11 @@
 
 #include <stdbool.h>
 
-#include "socketutil.h":
+#include "socketutil.h"
+
+void startListeningAndPrintingMessagesOnNewThread(int socketFD);
+void listenAndPrint(int socketFD);
+
 int main() {
     int socketFD = createTCPIPv4Socket();
 
@@ -15,28 +19,58 @@ int main() {
         perror("connection failed\n");
     }
 
+
+    startListeningAndPrintingMessagesOnNewThread(socketFD);
+
+
+    char *name = NULL;
+    size_t nameSize = 0;
+    printf("please enter your name\n");
+    ssize_t nameCount = getline(&name,&nameSize,stdin);
+    name[nameCount-1] = 0;
+
     char *line = NULL;
     size_t linesize = 0;
     printf("type and we will send(type exit)...\n");
 
+    char buffer[1024];
+
     while(true) {
         ssize_t charCount = getline(&line,&linesize,stdin);
+        line[charCount-1]=0;
+
+        sprintf(buffer,"%s:%s",name,line);
+
         if(charCount>0) {
             if(strcmp(line,"exit\n")==0)
                 break;
-            ssize_t amountWasSent = send(socketFD,line,charCount,0);
+            ssize_t amountWasSent = send(socketFD,
+                                        buffer,
+                                        strlen(buffer),
+                                        0);
         }
     }
-
-    char* message = "GET \\ HTTP/1.1\r\nHOST:google.com\r\n\r\n";
-    send(socketFD,message,strlen(message),0);
-
-    char buffer[1024];
-    recv(socketFD,buffer,1024,0);
-
-    printf("Resonse was %s\n",buffer);
 
     close(socketFD);
     return 0;
 }
+void startListeningAndPrintingMessagesOnNewThread(int socketFD) {
+    pthread_t id;
+    pthread_create(&id,NULL,listenAndPrint,socketFD);
 
+}
+
+void listenAndPrint(int socketFD) {
+    char buffer[1024];
+
+    while(true) {
+        ssize_t amountRecieved = recv(socketFD,buffer,1024,0);
+        if(amountRecieved>0) {
+            buffer[amountRecieved]=0;
+            printf("Response was %s",buffer);
+        }
+        if(amountRecieved==0)
+            break;
+    }
+    close(socketFD);
+}
